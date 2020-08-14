b3268d8a929b670029fd4edc694550b3a9c9722d
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-15 00:17:51 +0700

    Complete .

--- Multiples_of_3and5_WeekOne/Source.cpp
+++ Multiples_of_3and5_WeekOne/Source.cpp
@@ -1,10 +1,15 @@
 #include<stdio.h>
-
-
-
 int main()
 {
-
-
+    int sum, num1;
+    num1 = sum = 0;
+    for (num1 = 1; num1 < 1000; num1++)
+    {
+        if (((num1 % 3) == 0) || ((num1 % 5) == 0))
+        {
+            sum += num1;
+        }
+        printf("%d \n ", sum);
+    }
     return 0;
 }
